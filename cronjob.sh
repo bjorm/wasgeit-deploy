e6962b745ba665b44f599ac1057157beb7c07d75
Björn Mosler <dev@bjoern.mosler.ch>
2018-08-14 22:30:35 +0200

    Update cronjob.

--- cronjob.sh
+++ cronjob.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-@midnight cd /home/bjorm/wasgeit-deploy && /usr/local/bin/docker-compose -f prod.yml up crawler
+@midnight cd /home/bjorm/wasgeit-deploy && /usr/local/bin/docker-compose up crawler
