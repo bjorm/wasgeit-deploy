e7c939316a599397aff1e326bf97ab54ffed0a14
Björn Mosler <dev@bjoern.mosler.ch>
2018-12-08 22:58:29 +0100

    Move files into folders so docker-compose gets container names right.

--- cronjob.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-@midnight cd /home/bjorm/wasgeit-deploy && /usr/local/bin/docker-compose up crawler
---
+++ prod/cronjob.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+@midnight cd /home/bjorm/wasgeit-deploy/prod && /usr/local/bin/docker-compose run crawler
